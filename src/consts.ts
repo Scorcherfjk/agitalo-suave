4da40c6ed2939c72476b0319982c4a55cc6b0e74
Scorcherfjk <franjavikale@gmail.com>
2025-05-27 09:31:17 -0500

    fix: Correct spelling of SITE_TITLE from 'Agitalo Suave' to 'Agítalo Suave'

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Agitalo Suave';
+export const SITE_TITLE = 'Agítalo Suave';
 export const SITE_DESCRIPTION = 'Explorando el mundo de los cócteles, a mi ritmo';
