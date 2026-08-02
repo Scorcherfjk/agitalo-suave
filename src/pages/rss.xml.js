a17609a9ca7befc00e0723a2130e1b861d6b3067
Scorcherfjk <franjavikale@gmail.com>
2026-08-02 12:30:51 -0500

    feat: add RSS feed support and create robots.txt for SEO optimization

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,13 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: posts
+			.filter((post) => !post.data.draft)
+			.map((post) => ({
+				title: post.data.title,
+				link: `/blog/${post.id}/`,
+				pubDate: post.data.date,
+				description: post.data.excerpt,
+			})),
 	});
 }
